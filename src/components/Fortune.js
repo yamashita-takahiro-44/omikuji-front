1a96e97ab23ca1de6e3037e43dfb4fb170e3f4e8
yamashita-takahiro-44 <shimolen@gmail.com>
2023-12-31 08:11:17 +0900

    リリース

--- src/components/Fortune.js
+++ src/components/Fortune.js
@@ -61,7 +61,7 @@ const Fortune = () => {
           <img src={fortuneGif} alt="占い中" style={{ maxWidth: '100%', maxHeight: '100%' }} />
         </div>
       )}
-      <img src={logo} alt="ロゴ" style={{ width: '70%', left: '50%', bottom: 0, transform: 'translateX(-50%)', maxHeight: '500px', zIndex: 0 }} />
+      <img src={logo} alt="ロゴ" style={{ width: '70%', left: '50%', bottom: 0, transform: 'translateX(-50%)',position: 'absolute', maxHeight: '500px', zIndex: 0 }} />
     </Layout>
   );
 };
