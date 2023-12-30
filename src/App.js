ee5fe575e8bd44e346fff2bf36caa4a1079cadf0
yamashita-takahiro-44 <shimolen@gmail.com>
2023-12-31 02:08:58 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Fortune from './components/Fortune'; // Fortuneコンポーネントをインポート
 
@@ -7,20 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* Fortuneコンポーネントの追加 */}
         <Fortune />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/components/Fortune.js
+++ src/components/Fortune.js
@@ -1,33 +1,53 @@
 import React, { useState } from 'react';
+import { Input, Button, Card } from 'antd';
+import 'antd/dist/antd.css'; // AntDのスタイルをインポート
 
 const Fortune = () => {
   const [fortune, setFortune] = useState('');
+  const [prayer, setPrayer] = useState('');
 
   const getFortune = () => {
-    fetch('https://omikujiapp.fly.dev/fortunes')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log('API Response:', data); // レスポンスをログに出力
-        setFortune(data.fortune); // データをセット
-      })
-      .catch(error => {
-        console.error('Error fetching fortune:', error);
-      });
+    fetch('https://omikujiapp.fly.dev/fortunes', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ prayer })
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      setFortune(data.fortune);
+    })
+    .catch(error => {
+      console.error('Error fetching fortune:', error);
+    });
   };
 
   return (
-    <div>
-      <div>
-        <button onClick={getFortune}>Get Your Fortune</button>
-        {fortune && <p>{fortune}</p>}
-      </div>
-      <div>
-        <a href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(fortune)}`} target="_blank" rel="noopener noreferrer">Share on Twitter</a>
+    <div style={{ padding: '20px', maxWidth: '600px', margin: '0 auto' }}>
+      <Card title="2024年の運勢を占う" bordered={false} style={{ textAlign: 'center' }}>
+        <Input
+          placeholder="祈願の言葉を入力してください"
+          value={prayer}
+          onChange={e => setPrayer(e.target.value)}
+          style={{ marginBottom: '20px' }}
+        />
+        <Button type="primary" onClick={getFortune}>2024年の運勢を占う</Button>
+        {fortune && <p style={{ marginTop: '20px' }}>{fortune}</p>}
+      </Card>
+      <div style={{ marginTop: '20px', textAlign: 'center' }}>
+        {fortune && (
+          <a
+            href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(fortune + 'https://omikuji-front.vercel.app/')}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Share on Twitter
+          </a>
+        )}
       </div>
     </div>
   );
